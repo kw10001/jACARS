ee18afc49fdf810d3c18c57eb524a8cba28f72bb
Kylan Walters <kylanwalters@gmail.com>
2024-10-12 15:38:46 -0600

    initial commity for jACARS, a cross platform ACARS application for logging and tracking flight simulator flights. Uses data from both UIPC modules (FSUIPC, XUIPC) and from MSFS offsets.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame ();
+        frame.setSize(450,450);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        }
+    }
